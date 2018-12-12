909a2f988a57857f8600cc86d22e2ae3f1e85ac1
Meatless-Monster <mli8@wellesley.edu>
2018-12-12 04:07:28 +0000

    nav, show and index, add property route

---
+++ project/app.py
@@ -0,0 +1,170 @@
+import loft
+import bcrypt
+import MySQLdb
+
+from flask import (Flask, url_for, redirect, request, render_template, flash)
+
+app = Flask(__name__)
+
+app.secret_key = "Mb.Jp2u/6XT/)b`."
+
+
+@app.route('/start/', methods = ['POST', 'GET'])
+# For first time users to create an account
+# Would need to create an extra section for tenants
+def addUser():
+    if request.method == 'POST':
+        conn = loft.getConn('loft')
+        name = request.form.get('name')
+        email = request.form.get('email')
+        school = request.form.get('school')
+        pw = request.form.get('pw')
+        pw2 = request.form.get('pw_confirm')
+        valid = True
+        
+        if(email[-4:] != ".edu" or "@" not in email):
+            flash("Please enter a valid school email")
+            valid = False
+        if(pw != pw2):
+            flash("The passwords do not match")
+            valid = False
+        elif (len(pw) < 6 or any(char.isdigit() for char in pw) == False): #only checks when passwords match
+            flash("Password is too weak, must be longer than 6 characters and contain a digit")
+            valid = False
+        
+        # print valid
+        if valid == True:
+            hashed = bcrypt.hashpw(pw.encode('utf-8'), bcrypt.gensalt())
+            curs = conn.cursor(MySQLdb.cursors.DictCursor)
+            curs.execute('SELECT email FROM users WHERE email = %s', [email])
+            row = curs.fetchone()
+            if row is not None:
+                flash('An account with that email already exists')
+                return redirect(url_for('addUser'))
+            loft.createUser(conn, name, email, hashed, school)
+            return redirect(url_for('showProperties'))
+        else:
+            user = {
+                "name": name,
+                "email": email, 
+                "school": school,
+                "pw": pw,
+                "pw2": pw2
+            }
+            return render_template('account.html', user = user)
+    else:
+        return render_template('account.html')
+
+     
+#@app.route('/login/', methods=["POST"])
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        passwd = request.form['pw']
+        conn = loft.getConn('loft')
+        curs = conn.cursor(MySQLdb.cursors.DictCursor)
+        curs.execute('SELECT pw FROM users WHERE email = %s',
+                     [email])
+        row = curs.fetchone()
+        if row is None:
+            # Same response as wrong password, so no information about what went wrong
+            flash('login incorrect. Try again or join')
+            return redirect( url_for('login'))
+        hashed = row['pw']
+        # strings always come out of the database as unicode objects
+        if bcrypt.hashpw(passwd.encode('utf-8'),hashed.encode('utf-8')) == hashed:
+            flash('successfully logged in with '+email)
+            #session['username'] = username
+            #session['logged_in'] = True
+            #session['visits'] = 1
+            return redirect(url_for('showProperties'))
+        else:
+            flash('login incorrect. Try again or join')
+            return redirect( url_for('login'))
+    else:
+        return render_template('login.html')
+
+
+@app.route('/add-property/', methods = ["GET","POST"])
+# For first time users to create an account
+def addProperty():
+    if request.method == 'POST':
+        conn = loft.getConn('loft')
+        name = request.form.get('name')
+        descrip = request.form.get('descrip')
+        loc = request.form.get('location')
+        price = request.form.get('price')
+        smoker = request.form.get('smoker')
+        gender = request.form.get('gender')
+        pet = request.form.get('pet')
+        print((conn, name, descrip, loc, price, smoker, gender, pet))
+        loft.createProperty(conn, name, descrip, loc, price, smoker, gender, pet)
+        
+        # PID = loft.getLastProperty(conn)['PID']
+        # start = request.form.get('start_date') #as of now, assuming there is only one time period
+        # end = request.form.get('end_Date')
+        
+        # loft.createDate(conn, PID, start, end)
+        
+        return redirect(url_for('showProperties'))
+    else:
+        return render_template('addProp.html')
+
+@app.route('/', methods = ["GET","POST"])
+def showProperties():
+    conn = loft.getConn('loft')
+    if request.method == 'POST':
+        gender = int(request.form.get('gender'))
+        location = request.form.get('location')
+        price = request.form.get('price') #might use price ranges in the future
+        # THIS DOESN'T REALLY MAKE SENSE
+        # if price == "":
+        #     price = 100000 #no upper limit
+        propList = loft.searchProp(conn, gender, location, price)
+    else: 
+        propList = loft.getAll(conn) #shows all properties
+    return render_template('index.html', propList = propList)
+
+@app.route('/show/<id>', methods = ["GET"])
+def showPage(id):
+    #conn = loft.getConn('properties')
+    conn = loft.getConn('loft')
+    prop = loft.getOne(conn, id)
+    print ("TESTING: ", prop)
+    #return render_template('index.html', item = prop)
+    return render_template('show.html', item = prop)
+
+@app.route('/profile/<id>', methods = ["GET"])
+def profilePage(id):
+    conn = loft.getConn('loft')
+    profile = loft.getProfile(conn, id)
+    return render_template('profile.html', profile = profile)
+
+@app.route('/edit/<id>', methods = ["GET", "POST"])
+def edit(id):
+    if request.method == 'GET':
+        conn = loft.getConn('loft')
+        prop = loft.getOne(conn, id)
+        return render_template('edit.html', item = prop)
+    else:
+        conn = loft.getConn('loft')
+        name = request.form.get('name')
+        descrip = request.form.get('descrip')
+        loc = request.form.get('location')
+        price = request.form.get('price')
+        smoker = request.form.get('smoker')
+        gender = request.form.get('gender')
+        pet = request.form.get('pet')
+        loft.updateProperty(conn, id, name, descrip, loc, price, smoker, gender, pet)
+        return redirect(url_for('showPage', id = id))
+    
+@app.route('/delete/<id>', methods = ['GET', 'DELETE'])
+def delete(id):
+    conn = loft.getConn('loft')
+    loft.deleteProp(conn, id)
+    return redirect(url_for('showProperties'))
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run('0.0.0.0',8081)
---
+++ project/loft.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python2.7
+
+import sys
+import MySQLdb
+
+def getConn(db):
+    conn =  MySQLdb.connect(host='localhost',
+                           user='root',
+                           passwd='',
+                           db=db)
+    conn.autocommit(True)
+    return conn
+
+#--Adding to Database-- 
+def createUser(conn, name, email, pw, university):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''insert into users values (%s, %s, %s, %s, NULL)''',
+                (name, email, pw, university,))
+    return curs.fetchone()
+    
+def createProperty(conn, name, descrip, loc, price, smoker, gender, pet):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''insert into properties values (%s, %s, %s, %s, %s, %s, %s, NULL)''', 
+                (name, descrip, loc, price, smoker, gender, pet,))
+    return curs.fetchone()
+
+def createDate(conn, PID, start, end):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''insert into dates values (%s, %s, %s)''',
+                (PID, start, end,))
+    return curs.fetchone()
+
+def addTenantFeature(conn, UID, feature):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''insert into featuresTenants values (%s, %s)''',
+                (UID, feature,))
+    return curs.fetchone()
+
+def addPropertyFeature(conn, PID, feature):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''insert into featuresProperties values (%s, %s)''',
+                (PID, feature,))
+    return curs.fetchone()
+    
+def addHostProp(conn, UID, PID):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''insert into host_prop values (%s, %s)''',
+                (UID, PID,))
+    return curs.fetchone()
+
+# Searching properties based on specific filters
+def searchProp(conn, gender, location, price):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    if(gender == 3): #no preference
+        gender = "1, 2, 3 "
+    location = "%" + location + "%"
+    curs.execute('''select * from properties where propGender in (%s) and propLocation like %s and propPrice < %s''',
+                (gender, location, price))
+    return curs.fetchall()
+
+#updating table values
+def updateUser(conn, UID, name, email, pw, university):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''update users set name = %s, email = %s, pw = %s, university = %s where UID = %s''', 
+                (name, email, pw, university, UID))
+    return curs.fetchone
+def updateProperty(conn, PID, name, descrip, loc, price, smoker, gender, pet):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''update properties 
+                        set 
+                            propName = %s, 
+                            propDescription = %s, 
+                            propDescription = %s, 
+                            propPrice = %s, 
+                            propSmoker = %s, 
+                            propGender = %s, 
+                            propPet = %s 
+                        where 
+                            PID = %s''', 
+                (name, descrip, loc, price, smoker, gender, pet, PID))
+    return curs.fetchone
+
+#retrieves last property created based on largest PID value
+def getLastProperty(conn):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''select * from properties where PID = (select max(PID) from properties)''')
+    return curs.fetchone()
+
+def getAll(conn):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''select * from properties''')
+    return curs.fetchall()
+
+def getOne(conn, id):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''select * from properties where PID = %s''', [id])
+    return curs.fetchone()
+    
+def getProfile(conn, id):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''select * from users where UID = %s''', [id])
+    return curs.fetchone()
+    
+def deleteProp(conn, id):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''delete from properties where PID = %s''', [id])
+    return 
+
+if __name__ == '__main__':
+    conn = getConn('loft')
+    # user = createUser(conn, 'Ally', 'ally@tufts.edu', 'Password123', 'Tufts University')
+    prop = createProperty(conn, 'House', 'A House in Boston', 'Boston', 800, 0, 1, 0)
+    print(searchProp(conn,3,"",10000))
